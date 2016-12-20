5796b7c8504440972e97e2b55e55b81c04f29dd6
Marco Ghilardelli <marco.ghilardelli@students.fhnw.ch>
2016-12-20 10:13:37 +0100

    add a google maps

--- js/script.js
+++ js/script.js
@@ -7,6 +7,13 @@ $(document).ready(function () {
     });
 });
 
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map_wrapper'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+}
 
 $(function () {
     Highcharts.chart('chartContainer', {
