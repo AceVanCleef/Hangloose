74b17c9218bfb3b0d68b3bc455a69e1ce61c6261
Marco Ghilardelli <marco.ghilardelli@students.fhnw.ch>
2017-01-10 21:18:57 +0100

    sample

---
+++ src/api/api_config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: degonas
+ * Date: 04.01.2017
+ * Time: 15:21
+ */
+
+define('QUERY_STRING', 'mysql:host=localhost;dbname=hangloose;charset=utf8');
+define('DB_USER', 'webec');
+define('DB_PWD', 'webec');
