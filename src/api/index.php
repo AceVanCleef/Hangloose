33d0291db45533bb07d2cb4827582d93c34a3ab4
Stefan Wohlgensinger <stefan.wohlgensinger@students.fhnw.ch>
2017-01-14 21:58:46 +0100

    index.php: removes Übung 10 left over code.

--- src/api/index.php
+++ src/api/index.php
@@ -13,33 +13,12 @@
  * */
 
 
-define('TIME_ZONE', 'UTC');
-define('DATE_FORMAT', 'd\.m\.Y');
-define('DATE_FIELD_NAME', 'date');
-
 $app = new \Slim\App();
-
-
-/**
- * Die SLIM Routes.
- */
-$id = array();
-
 
 
     /**
      * Die SLIM Routes.
      */
-    $id = array();
-
-    /**
-     *  Test query
-     */
-    $app->get('/test/{nr}', function($request, $response, $args) {
-
-        echo('{response: "hallo"}');
-    });
-
 
 
 $app->get('/ratings/{lat}/{lng}', function ($request, $response, $args) {
@@ -295,17 +274,6 @@ $app->get('/ratings/{lat}/{lng}', function ($request, $response, $args) {
         return $stmt;
     }
 
-//--------------------------------------------------------------------------------------------------
-$app->get('/film-quote/{id}', function ($request, $response, $args) {
-
-    return getRating($response, "SELECT * FROM quotes WHERE id = {$args['id']}");
-});
-
-$app->post('/film-quotes', function ($request, $response) {
-
-    return createFilmQuote($response, $request->getParsedBody());
-});
-
 
 /**
  * Gibt eine Datenbank-Verbindung zurück. Falls das nicht gelingt, wird die SLIM App mit einem
@@ -356,61 +324,6 @@ function getRatings($response, $args)
     }
 }
 
-
-/**
- * Bindet die JSON-Daten in das Prepared Statement.
- *
- * @param $preparedStatement  Das Prepared-Statement
- * @param $jsonData  Film-Quote als JSON Struktur
- * @return  Das Prepared-Statement den Daten
- */
-function bindParameters($preparedStatement, $jsonData)
-{
-
-    $preparedStatement->bindParam(':title', $jsonData['title'], PDO::PARAM_STR);
-    $preparedStatement->bindParam(':quote', $jsonData['quote'], PDO::PARAM_STR);
-    $preparedStatement->bindParam(':movie_character', $jsonData['movie_character'], PDO::PARAM_STR);
-    $preparedStatement->bindParam(':actor', $jsonData['actor'], PDO::PARAM_STR);
-    $preparedStatement->bindParam(':year', $jsonData['year'], PDO::PARAM_INT);
-
-    return $preparedStatement;
-}
-
-
-/**
- * Erzeugt einen Film-Quote Eintrag in der Datenbank. Gibt dem Aufrufer die JSON Struktur ergänzt mit der ID,
- * die zum Abfragen nötig ist zurück. Status Code ist 201 Created. Falls etwas schief läuft wird der Request mit
- * Status 500 zurück gegeben.
- *
- * @param $response  Das Response Object
- * @param $jsonData  Die Film-Quote als JSON
- * @return Das Response Object
- */
-function createFilmQuote($response, $jsonData)
-{
-
-    try {
-
-        $db = getDBConnection($response);
-
-        $stmt = $db->prepare('INSERT INTO quotes (title, quote, movie_character, actor, year) values(:title, :quote, :movie_character, :actor, :year)');
-
-        $stmt = bindParameters($stmt, $jsonData);
-
-        if ($stmt->execute()) {
-
-            $jsonData['id'] = $db->lastInsertId();
-
-            return $response->withJson($jsonData, 201);
-        } else {
-
-            return $response->write('Error inserting in database.')->withStatus(500);
-        }
-    } catch (Exception $e) {
-
-        return $response->write($e->getMessage())->withStatus(503);
-    }
-}
 
 /**
  * Startet die SLIM App. Muss zuletzt im Skript aufgerufen werden.
